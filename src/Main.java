13b71e903b8d22c6902915ca739d1e68fe0f5078
Cameron <morelacn@mail.uc.edu>
2023-09-13 17:44:12 -0400

    Completed (For real this time)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         double balance = 5000;
+        double interest;
 
         for (int x = 1; x <= 2; x = x + 1) {
             balance = balance * 1.17;
-            System.out.println("Your balance after " + x + " month(s) is $" + balance);
+            interest = balance - 5000;
+            System.out.println("Your balance after " + x + " month(s) is $" + balance + ". Interest owed is $" + interest + ".");
         }
     }
 }
